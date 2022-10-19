58f7f073ccbec2f52ab36ecdc454f14295a686d7
Ruslan Galiev <v-rgaliev@microsoft.com>
2022-10-20 00:28:47 +0200

    Added IdentityServer project

---
+++ IdentityServer/Program.cs
@@ -0,0 +1,35 @@
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace IdentityServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddIdentityServer()
+                .AddInMemoryClients(Config.Clients)
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddInMemoryIdentityResources(Config.IdentityResources)
+                .AddTestUsers(Config.TestUsers)
+                .AddDeveloperSigningCredential();
+
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseIdentityServer();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
--- Movies.API/Program.cs
+++ Movies.API/Program.cs
@@ -37,8 +37,6 @@ namespace Movies.API
 
             app.MapControllers();
 
-            
-
             app.Run();
         }
 
